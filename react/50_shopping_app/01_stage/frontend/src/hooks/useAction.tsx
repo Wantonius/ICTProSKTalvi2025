2b9eae6087a5f20d08e4d677d0dc8a5be924c81a
Erno Hentonen <erno.hentonen@opiframe.com>
2025-01-14 13:56:07 +0200

    shopping app backend 1

--- react/50_shopping_app/01_stage/frontend/src/hooks/useAction.tsx
+++ react/50_shopping_app/01_stage/frontend/src/hooks/useAction.tsx
@@ -22,6 +22,101 @@ const useAction = () => {
 	const [state,setState] = useState<AppState>({
 		list:[]
 	})
+	
+	//Effect gets triggered by changes in request. This is done by API functions which
+	//correspond to backend functionalities. So we will have getList,add,remove and edit
+	//API functions which trigger the fetch async function with correct parameters for 
+	//each call.
+	
+	useEffect(() => {
+		
+		const fetchData = async () => {
+			if(urlRequest.action === "") {
+				return;
+			}
+			const response = await fetch(urlRequest.request)
+			if(!response) {
+				console.log("Server sent no response")
+				return;
+			}
+			if(response.ok) {
+				switch(urlRequest.action) {
+					case "getlist":
+						let data = await response.json();
+						if(!data) {
+							console.log("Failed to parse json");
+							return;
+						}
+						setState({
+							list:data
+						})
+						return;
+					case "additem":
+					case "removeitem":
+					case "edititem":
+						getList();
+						return;
+					default:
+						return;
+				}
+			} else {
+				console.log("Server responded with a status "+response.status+" "+response.statusText)
+			}
+		}
+		
+		fetchData();
+		
+	},[urlRequest])
+
+	//API FUNCTIONS
+	
+	const getList = () => {
+		setUrlRequest({
+			request:new Request("/api/shopping",{
+				method:"GET"
+			}),
+			action:"getlist"
+		})
+	}
+	
+	const add = (item:ShoppingItem) => {
+		setUrlRequest({
+			request:new Request("/api/shopping",{
+				method:"POST",
+				headers:{
+					"Content-type":"application/json"
+				},
+				body:JSON.stringify(item)
+			}),
+			action:"additem"
+		})
+	}
+	
+	const remove = (id:number) => {
+		setUrlRequest({
+			request:new Request("/api/shopping/"+id,{
+				method:"DELETE"
+			}),
+			action:"removeitem"
+		})
+	}
+	
+	const edit = (item:ShoppingItem) => {
+		setUrlRequest({
+			request:new Request("/api/shopping/"+item.id,{
+				method:"PUT",
+				headers:{
+					"Content-Type":"application/json"
+				},
+				body:JSON.stringify(item)
+			}),
+			action:"edititem"
+		})
+	}
+	
+	//We return the state and API functions to be used by components
+	
+	return {state,getList,add,remove,edit}
 }
 
 export default useAction;
