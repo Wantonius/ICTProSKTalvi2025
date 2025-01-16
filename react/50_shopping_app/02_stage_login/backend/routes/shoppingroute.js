b275f3d0403088aaf12878ebf4373f2ebc9b27a2
Erno Hentonen <erno.hentonen@opiframe.com>
2025-01-16 12:18:33 +0200

    login frontend

--- react/50_shopping_app/02_stage_login/backend/routes/shoppingroute.js
+++ react/50_shopping_app/02_stage_login/backend/routes/shoppingroute.js
@@ -1,22 +1,25 @@
 const express = require("express");
-
+const itemModel = require("../models/item");
 const router = express.Router();
 
-//DATABASE
-
-let database = [];
-let id = 100;
 
 //API
 
 //Get database, return in json form
 
 router.get("/shopping",function(req,res) {
-	return res.status(200).json(database);
+	let query = {"user":req.session.user}
+	itemModel.find(query).then(function(items){
+		return res.status(200).json(items);
+	}).catch(function(err) {
+		console.log("Failed to find items for user "+req.session.user+". Reason",err);
+		return res.status(500).json({"Message":"Internal Server Error"});
+	})
 })
 
 /* Add new item to database. Item comes in JSON Form
 {
+	user:string,
 	type:string,
 	count:number,
 	price:number
@@ -26,43 +29,57 @@ return status 201 Created and the new item
 */
 
 router.post("/shopping",function(req,res) {
-	let item = {
-		id:id,
+	if(!req.body) {
+		return res.status(400).json({"Message":"Bad request"})
+	}
+	if(!req.body.type) {
+		return res.status(400).json({"Message":"Bad request"})
+	}
+	let item = new itemModel({
+		user:req.session.user,
 		type:req.body.type,
 		count:req.body.count,
 		price:req.body.price
-	}
-	id++;
-	database.push(item);
-	return res.status(201).json(item);
+	})
+	item.save().then(function() {
+		return res.status(201).json({"Message":"Created"})
+	}).catch(function(err) {
+		console.log("Failed to create new item. Reason",err);
+		return res.status(500).json({"Message":"Internal server error"})
+	});
 })
 
-/* Remove item using id. Id will be a parameter in the url */
+/* Remove item using _id. Id will be a parameter in the url */
 
 router.delete("/shopping/:id",function(req,res) {
-	let tempId = parseInt(req.params.id);
-	let tempDatabase = database.filter(item => item.id !== tempId);
-	database = tempDatabase;
-	return res.status(200).json({"Message":"Success"})
+	itemModel.deleteOne({"_id":req.params.id,"user":req.session.user}).then(function() {
+		return res.status(200).json({"Message":"Success"})
+	}).catch(function(err) {
+		return res.status(500).json({"Message":"Internal server error"})
+	})
 })
 
-//Update item using id. We use Array.prototype.splice to change the existing item to new one.
+//Update item using _id. We use mongooses Model.replaceOne to replace current with new.
 
 router.put("/shopping/:id",function(req,res) {
-	let tempId = parseInt(req.params.id);
+	if(!req.body) {
+		return res.status(400).json({"Message":"Bad request"})
+	}
+	if(!req.body.type) {
+		return res.status(400).json({"Message":"Bad request"})
+	}
 	let item = {
-		id:tempId,
+		user:req.session.user,
 		type:req.body.type,
 		count:req.body.count,
 		price:req.body.price
 	}
-	for(let i=0;i<database.length;i++) {
-		if(tempId === database[i].id) {
-			database.splice(i,1,item);
-			return res.status(200).json({"Message":"Success"})
-		}
-	}
-	return res.status(404).json({"Message":"Not found"})
+	itemModel.replaceOne({"_id":req.params.id,"user":req.session.user},item).then(function() {
+		return res.status(200).json({"Message":"Success"})
+	}).catch(function(err) {
+		console.log("Failed to edit item _id "+req.params.id+". Reason",err);
+		return res.status(500).json({"Message":"Internal server error"})
+	})
 })
 
 module.exports = router;
