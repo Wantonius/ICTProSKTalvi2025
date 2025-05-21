8f3fbd615401d8991d60a56792af4d0c988afb8e
Erno Hentonen <erno.hentonen@opiframe.com>
2025-05-21 12:44:50 +0300

    composition

---
+++ react/15_composition/src/components/ContactInfo.tsx
@@ -0,0 +1,17 @@
+interface Props {
+	name:string;
+	profession:string;
+}
+
+const ContactInfo = (props:Props) => {
+
+	return(
+		<>
+			<p>{props.name}</p>
+			<p>{props.profession}</p>
+		</>
+	)
+
+}
+
+export default ContactInfo;
